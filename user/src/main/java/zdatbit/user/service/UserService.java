3ebb455fac2841cc7e8d9793fbadb7ea36dd9115
zhangdi <zdatbit@163.com>
2018-01-10 22:04:20 +0800

    家里环境

--- user/src/main/java/zdatbit/user/service/UserService.java
+++ user/src/main/java/zdatbit/user/service/UserService.java
@@ -6,9 +6,11 @@ import zdatbit.user.service.base.TemplateService;
 import zdatbit.user.dao.TemplateDao;
 import zdatbit.user.domain.User;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,6 +25,27 @@ public class UserService extends TemplateService {
 
     public void sayHello() {
         System.out.print("hello world");
+    }
+
+    @PostConstruct
+    public boolean initUsers(){
+        List<User> list = new ArrayList<User>();
+
+        User user0 = new User();
+        user0.setUserName("zdatbit");
+        user0.setUserPassword("123456");
+        list.add(user0);
+
+        User user1 = new User();
+        user1.setUserName("zda");
+        user1.setUserPassword("123456");
+        list.add(user1);
+        try {
+            return dao.saveBatch(list);
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public List<User> selectAll() {
