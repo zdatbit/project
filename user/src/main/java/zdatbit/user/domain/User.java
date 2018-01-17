740aa1ae8136f2ae1e278eaf0b25984be0e4365f
zhangdi <zdatbit@163.com>
2018-01-17 16:25:51 +0800

    hibernate不能生成数据库，改用更高版本的hibernate，用注解方式来生成

--- user/src/main/java/zdatbit/user/domain/User.java
+++ user/src/main/java/zdatbit/user/domain/User.java
@@ -1,28 +1,22 @@
 package zdatbit.user.domain;
 
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-
+@Entity
+@Table(name="user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 401070369244739620L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer userID;
 	private String userName;
-	private String userPassword;
-	private String salt2;
-	private String realName;
-	private String userEmail;
 	private String userPhone;
-	private String company;
-	private String department;
-	private Integer userStatus;
-	private Boolean isOnline;
-	private Date lastLogin;
-	private Date createTime;
 	public Integer getUserID() {
 		return userID;
 	}
@@ -35,72 +29,12 @@ public class User implements Serializable{
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-	public String getSalt2() {
-		return salt2;
-	}
-	public void setSalt2(String salt2) {
-		this.salt2 = salt2;
-	}
-	public String getRealName() {
-		return realName;
-	}
-	public void setRealName(String realName) {
-		this.realName = realName;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
+
 	public String getUserPhone() {
 		return userPhone;
 	}
+
 	public void setUserPhone(String userPhone) {
 		this.userPhone = userPhone;
 	}
-	public String getCompany() {
-		return company;
-	}
-	public void setCompany(String company) {
-		this.company = company;
-	}
-	public String getDepartment() {
-		return department;
-	}
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-	public Integer getUserStatus() {
-		return userStatus;
-	}
-	public void setUserStatus(Integer userStatus) {
-		this.userStatus = userStatus;
-	}
-	public Boolean getIsOnline() {
-		return isOnline;
-	}
-	public void setIsOnline(Boolean isOnline) {
-		this.isOnline = isOnline;
-	}
-	public Date getLastLogin() {
-		return lastLogin;
-	}
-	public void setLastLogin(Date lastLogin) {
-		this.lastLogin = lastLogin;
-	}
-	public Date getCreateTime() {
-		return createTime;
-	}
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-
-	
 }
